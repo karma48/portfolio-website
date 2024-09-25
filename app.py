a033720df17eed6be90b53635307e975cfcf4139
Karma48 <166092717+karma48@users.noreply.github.com>
2024-09-26 00:09:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ with tabs[0]:
     col1, col2 = st.columns([1, 2])
     
     with col1:
-        st.image("Profile Pic.png", use_column_width=True)  # Replace with your image URL
+        st.image("photo.png", use_column_width=True)  # Replace with your image URL
     
     with col2:
         st.write("""
